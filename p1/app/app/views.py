f22a7856c0c936e4caaaea264b5c3b82c279e4b4
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-11-28 01:40:25 +0530

    complete project 1

--- p1/app/app/views.py
+++ p1/app/app/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 
 def home(request):
-    return HttpResponse("Welcome to the Home Page!")
+    return render(request, 'home.html')
 def about(request):
-    return HttpResponse("This is the About Page.")
+    return render(request, 'about.html')
 def contact(request):
-    return HttpResponse("Contact us at contact@example.com")
+    return render(request, 'contact.html')
