9b55d09cd43a559edbfc1a153b75433a570cabed
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-11-29 15:23:15 +0530

    install tailwind to python

--- p2/myproject/myproject/urls.py
+++ p2/myproject/myproject/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('chai/', include('app.urls')),
+
+
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
