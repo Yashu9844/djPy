7dff4221f8ecda281f2d14d4c0d0156a716d8603
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-12-01 13:47:54 +0530

    feat:add layout and models to the projkect

--- p2/myproject/app/models.py
+++ p2/myproject/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Chai(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='chai_images/')
+    date_added = models.DateTimeField(auto_now_add=timezone.now)
+    CHAI_TYPE = (
+        ('ML', 'Milk'),
+        ('GR', 'Grass'),
+        ('CH', 'Chai'),
+    )
+    type = models.CharField(max_length=2, choices=CHAI_TYPE, default='ML')
