6bdb7cd8447c2d5110985161808afceb6bb4dbaf
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-12-02 15:10:17 +0530

    realationships and admin

--- p2/myproject/app/admin.py
+++ p2/myproject/app/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Chai
-admin.site.register(Chai)
+from .models import Chai, ChaiReview , Store, ChaiCertification
+
+
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'type', 'date_added')
+    inlines = [ChaiReviewInline]
+    
+class storeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chais',)
+
+class ChaiCertificationAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certified_by', 'certification_date', 'valid_until')
+
+
+admin.site.register(Chai,ChaiAdmin)
+admin.site.register(ChaiReview)
+admin.site.register(Store,storeAdmin)
+admin.site.register(ChaiCertification,ChaiCertificationAdmin)
