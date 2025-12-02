b419523cc6838e421e0800af1f45e8c723294176
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-12-02 14:55:21 +0530

    feat:added relations

--- p2/myproject/app/models.py
+++ p2/myproject/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Chai(models.Model):
@@ -16,3 +17,35 @@ class Chai(models.Model):
     def __str__(self):
         return self.name
 
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(Chai, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_reviewed = models.DateTimeField(auto_now_add=timezone.now)
+
+    def __str__(self):
+        return f'Review for {self.chai.name} by {self.user.username}'
+    
+
+## Many to Many
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    chais = models.ManyToManyField(Chai, related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+
+
+## One to One
+
+class ChaiCertification(models.Model):
+    chai = models.OneToOneField(Chai, on_delete=models.CASCADE, related_name='certification')
+    certified_by = models.CharField(max_length=100)
+    certification_date = models.DateField(default=timezone.now)
+    valid_until = models.DateField()
+    def __str__(self):
+        return f'Certification for {self.chai.name} by {self.certified_by}'
